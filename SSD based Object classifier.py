df1df5216e7860ad4ec3db0bcd2d66135da76b1c
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-03-18 23:08:35 +0530

    Update SSD based Object classifier.py

--- SSD based Object classifier.py
+++ SSD based Object classifier.py
@@ -24,8 +24,7 @@ def detect(frame, net, transform):
             pt=(detections[0,i,j,1:]*scale).numpy()
             cv2.rectangle(frame, (int(pt[0]),int(pt[1])),(int(pt[2]),int(pt[3])),(255,0,0),2)
             cv2.putText(frame,labelmap[i-1],(int(pt[0]),int(pt[1])),cv2.FONT_HERSHEY_SIMPLEX,2 ,(255,255,255),2,cv2.LINE_AA)
-            j=j+1
-            
+            j=j+1     
     return frame
   
 net=build_ssd('test')
