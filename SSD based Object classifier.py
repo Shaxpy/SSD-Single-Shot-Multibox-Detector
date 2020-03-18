56e292e81ccb1fd723f71f7218179d2bd738bd9d
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-03-18 23:09:31 +0530

    Update SSD based Object classifier.py

--- SSD based Object classifier.py
+++ SSD based Object classifier.py
@@ -34,7 +34,6 @@ reader = imageio.get_reader('funny_dog.mp4')
 fps = reader.get_meta_data()['fps']
 writer=imageio.get_writer('output.mp4', fps=fps)
 
-
 for i,frame in enumerate(reader):
      frame = detect(frame,net.eval(),transform)
      writer.append_data(frame)
